8a046c3fb2e2ddde85171422963abf8e0986d90d
Raymond Li <raymond@raymond-ubuntu-18.04>
2019-10-24 20:57:09 -0700

    added LSTM models

--- models/EncoderRNN_LSTM.py
+++ models/EncoderRNN_LSTM.py
@@ -4,24 +4,6 @@ import pdb
 
 
 class EncoderRNN(nn.Module):
-    def __init__(self, weights, emb_size, hidden_size, n_layers, dropout_p):
-        super(EncoderRNN, self).__init__()
-
-        self.hidden_size = hidden_size
-        self.n_layers = n_layers
-        self.embedding = nn.Embedding.from_pretrained(weights)
-        self.gru = nn.GRU(emb_size, hidden_size, n_layers, dropout=dropout_p)
-
-    def forward(self, input, hidden):
-        embedded = self.embedding(input).view(1, 1, -1)
-        output = embedded
-        output, hidden = self.gru(output, hidden)
-        return output, hidden
-
-    def initHidden(self, device):
-        return torch.zeros(self.n_layers, 1, self.hidden_size, device=device)
-
-class EncoderRNNLSTM(nn.Module):
     def __init__(self, weights, emb_size, hidden_size, n_layers):
         super(EncoderRNN, self).__init__()
 
