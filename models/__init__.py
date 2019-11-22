2fad28f090344fc237c1cde99ba73398faf81ed7
raymondzmc <li.raymond96@gmail.com>
2019-11-21 16:43:20 -0800

    added init.py

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from AttnDecoderRNN import *
+from AttnDecoderRNN_bi import *
+from AttnDecoderRNN_LSTM import *
+from EncoderRNN import *
+from EncoderRNN_bi import *
+from EncoderRNN_LSTM import *
