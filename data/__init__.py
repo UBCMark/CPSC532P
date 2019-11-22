80df73f3d5fff5fe51a9e7a9bbb0eaf150b4d167
raymondzmc <li.raymond96@gmail.com>
2019-11-21 16:57:07 -0800

    added __init__.py

--- data/__init__.py
+++
@@ -1,2 +0,0 @@
-from dataset import SummarizationDataset, get_dataloader
-import cfg
--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from AttnDecoderRNN import *
-from AttnDecoderRNN_bi import *
-from AttnDecoderRNN_LSTM import *
-from EncoderRNN import *
-from EncoderRNN_bi import *
-from EncoderRNN_LSTM import *
